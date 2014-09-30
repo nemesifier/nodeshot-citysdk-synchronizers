06775d96be633e95c4a694d251e6f1bf437eb938
Federico Capoano <nemesis@ninux.org>
2014-09-30 18:42:19 +0200

    Added __init__

---
+++ nodeshot_citysdk_synchronizers/__init__.py
@@ -0,0 +1,26 @@
+VERSION = (0, 1, 0, 'alpha', 0)
+__version__ = VERSION
+
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s' % (version, VERSION[3])
+    return version
+
+
+from .citysdk_mobility import CitySdkMobility
+from .citysdk_tourism import CitySdkTourism
+from .geojson_citysdk_mobility import GeoJsonCitySdkMobility
+from .geojson_citysdk_tourism import GeoJsonCitySdkTourism
+from .openwisp_citysdk_tourism import OpenWispCitySdkTourism
+from .provinciawifi import ProvinciaWifi
+from .provinciawifi_citysdk_mobility import ProvinciaWifiCitySdkMobility
+from .provinciawifi_citysdk_tourism import ProvinciaWifiCitySdkTourism
+from .provincerometraffic import ProvinceRomeTraffic
+from .openlabor import OpenLabor
