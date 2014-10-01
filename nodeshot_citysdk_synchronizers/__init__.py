db6b0fdb54457111ca3592a262f90132ddedc09f
Federico Capoano <nemesis@ninux.org>
2014-10-01 14:59:24 +0200

    Catch exception in case running "python setup.py"

--- nodeshot_citysdk_synchronizers/__init__.py
+++ nodeshot_citysdk_synchronizers/__init__.py
@@ -14,14 +14,18 @@ def get_version():
     return version
 
 
-from .settings import NODESHOT_SYNCHRONIZERS
-from .citysdk_mobility import CitySdkMobility
-from .citysdk_tourism import CitySdkTourism
-from .geojson_citysdk_mobility import GeoJsonCitySdkMobility
-from .geojson_citysdk_tourism import GeoJsonCitySdkTourism
-from .openwisp_citysdk_tourism import OpenWispCitySdkTourism
-from .provinciawifi import ProvinciaWifi
-from .provinciawifi_citysdk_mobility import ProvinciaWifiCitySdkMobility
-from .provinciawifi_citysdk_tourism import ProvinciaWifiCitySdkTourism
-from .provincerometraffic import ProvinceRomeTraffic
-from .openlabor import OpenLabor
+try:
+    from .settings import NODESHOT_SYNCHRONIZERS
+except:
+    pass
+else:
+    from .citysdk_mobility import CitySdkMobility
+    from .citysdk_tourism import CitySdkTourism
+    from .geojson_citysdk_mobility import GeoJsonCitySdkMobility
+    from .geojson_citysdk_tourism import GeoJsonCitySdkTourism
+    from .openwisp_citysdk_tourism import OpenWispCitySdkTourism
+    from .provinciawifi import ProvinciaWifi
+    from .provinciawifi_citysdk_mobility import ProvinciaWifiCitySdkMobility
+    from .provinciawifi_citysdk_tourism import ProvinciaWifiCitySdkTourism
+    from .provincerometraffic import ProvinceRomeTraffic
+    from .openlabor import OpenLabor
