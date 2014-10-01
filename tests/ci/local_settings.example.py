5cc60dd1f56d8325b24e5639311e85c49237cd61
Federico Capoano <nemesis@ninux.org>
2014-10-01 14:48:29 +0200

    Added test project

---
+++ tests/ci/local_settings.example.py
@@ -0,0 +1,18 @@
+NODESHOT_CITYSDK_TOURISM_TEST_CONFIG = {
+    "citysdk_category": "Testing Layer",
+    "citysdk_category_id": "IDHERE",
+    "citysdk_lang": "it-IT",
+    "citysdk_password": "password",
+    "citysdk_term": "center",
+    "citysdk_type": "poi",
+    "citysdk_url": "http://HOST/",
+    "citysdk_username": "admin",
+    "search_url": "http://HOST/pois/search"
+}
+
+NODESHOT_CITYSDK_MOBILITY_TEST_CONFIG = {
+    "citysdk_url": "https://HOST/",
+    "citysdk_username": "USER",
+    "citysdk_password": "PASSWORD",
+    "citysdk_layer": "testing.layer.name",
+}
---
+++ tests/ci/settings.py
@@ -0,0 +1,139 @@
+import os
+
+
+# ------ BEGIN DON'T TOUCH AREA ------ #
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+SECRET_KEY = '_1%+df^n-3py*u+r@r_1#i8pj&7@w4!0#4pw=w=s2a$z7y^%c7'
+
+ROOT_URLCONF = 'ci.urls' #
+
+WSGI_APPLICATION = 'ci.wsgi.application'
+
+# ------ END DON'T TOUCH AREA ------ #
+
+
+DEBUG = True
+DOMAIN = '127.0.0.1'
+SITE_NAME = 'ci'  # site name, you can change this
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'nodeshot_citysdk_ci',
+        'USER': 'postgres',
+        'PASSWORD':  'password',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    },
+}
+
+# import the default nodeshot settings
+# do not move this import
+from nodeshot.conf.settings import *
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.gis',
+    # hstore support
+    'django_hstore',
+    # admin site
+    'grappelli.dashboard',
+    'grappelli',
+    'filebrowser',
+    'django.contrib.admin',
+    # celery django email backend
+    'djcelery_email',
+    # nodeshot
+    'nodeshot.core.api',
+    'nodeshot.core.layers',
+    'nodeshot.core.nodes',
+    'nodeshot.interop.sync',
+    'nodeshot_citysdk_synchronizers',
+    # 3d parthy django apps
+    'rest_framework',
+    'rest_framework_swagger',
+    'leaflet',
+    'south',
+    'smuggler',
+    'reversion',
+    'corsheaders',
+    # other utilities
+    'django_extensions',
+]
+
+AUTH_USER_MODEL = 'auth.User'
+
+# ------ All settings customizations must go here ------ #
+
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Europe/Amsterdam'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-gb'
+
+ADMINS = (
+    #('Your name', 'your@email.com'),
+)
+
+MANAGERS = ADMINS
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'root@localhost'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER  # used for error reporting
+
+LEAFLET_CONFIG.update({
+    'DEFAULT_CENTER': (49.06775, 30.62011),
+    'DEFAULT_ZOOM': 4,
+    'MIN_ZOOM': 1,
+    'MAX_ZOOM': 18,
+    'TILES': 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png',
+})
+
+# social auth
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''
+GOOGLE_OAUTH2_CLIENT_ID = ''
+GOOGLE_OAUTH2_CLIENT_SECRET = ''
+GITHUB_APP_ID = ''
+GITHUB_API_SECRET = ''
+
+#from datetime import timedelta
+#
+#CELERYBEAT_SCHEDULE.update({
+#    'synchronize': {
+#        'task': 'nodeshot.interop.sync.tasks.synchronize_external_layers',
+#        'schedule': timedelta(hours=12),
+#    },
+#    # example of how to synchronize one of the layers with a different periodicity
+#    'synchronize': {
+#        'task': 'nodeshot.interop.sync.tasks.synchronize_external_layers',
+#        'schedule': timedelta(minutes=30),
+#        'args': ('layer_slug',)
+#    },
+#    # example of how to synchronize all layers except two layers
+#    'synchronize': {
+#        'task': 'nodeshot.interop.sync.tasks.synchronize_external_layers',
+#        'schedule': timedelta(hours=12),
+#        'kwargs': { 'exclude': 'layer1-slug,layer2-slug' }
+#    }
+#})
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
+try:
+    from local_settings import *
+except:
+    pass
